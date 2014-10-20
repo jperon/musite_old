5cfd26dbe0712201a01d2502e5a4b2d64b2f8328
jperon <cataclop@hotmail.com>
2014-10-20 18:57:44 +0200

    Avancement de l'interface pour gregorio.

--- lib/plugins/gregorio.py
+++ lib/plugins/gregorio.py
@@ -31,19 +31,26 @@ def titregabc(f):
 def accueillir():
     racine = os.path.join(c.DATA,EXT)
     dossiers = os.walk(racine)
-    structure = '\n'.join(
-        ['<b>{0}</b>\n<ul id="listechants">\n\t<li>{1}</li>\n</ul>'.format(
-            dossier.replace(racine + os.sep,'').capitalize(),
-            '</li>\n\t<li>'.join(
-                '<a href="/gregorio/afficher/?fichier={0}">{1}</a>'.format(
-                    os.path.join(dossier.replace(racine + os.sep,''),fichier),
-                    titregabc(os.path.join(dossier,fichier))
-                    )
-                for fichier in sorted(fichiers,key=lambda x: x.lower())),
+    page = (
+        s.authentifie(
+            '<div id="actions">'
+            + '<a href="/gregorio/creer/">Créer une nouvelle pièce</a>\n<br>\n'
+            + '</div>'
             )
-        for dossier, sousdossiers, fichiers in sorted(dossiers) if len(fichiers)]
+        + '\n'.join(
+            ['<b>{0}</b>\n<ul id="listechants">\n\t<li>{1}</li>\n</ul>'.format(
+                dossier.replace(racine + os.sep,'').capitalize(),
+                '</li>\n\t<li>'.join(
+                    '<a href="/gregorio/afficher/?fichier={0}">{1}</a>'.format(
+                        os.path.join(dossier.replace(racine + os.sep,''),fichier),
+                        titregabc(os.path.join(dossier,fichier))
+                        )
+                    for fichier in sorted(fichiers,key=lambda x: x.lower())),
+                )
+            for dossier, sousdossiers, fichiers in sorted(dossiers) if len(fichiers)]
+            )
         )
-    return structure
+    return page
 
 @s.page
 def afficher(parametres):
@@ -62,9 +69,9 @@ def afficher(parametres):
                         '/'.join(sousdossier),fichierpdf
                         ),
                 actions = (
-                    'Télécharger '
+                    'Mettre en page'
                     + s.authentifie(
-                        '<a href=/gregorio/editer/?fichier={}>Éditer</a>'.format(
+                        ' − <a href=/gregorio/editer/?fichier={}>Éditer</a>'.format(
                             parametres['fichier']
                             )
                         )
@@ -72,8 +79,12 @@ def afficher(parametres):
                 )
 
 @s.page
-def telecharger(parametres):pass
-    #compiler()
+def creer(parametres):
+    with open(os.path.join(DOSSIER,'creation.html')) as f:
+        return Template(f.read(-1)).substitute(
+            nom = NOM,
+            )
+    pass
 
 @s.page
 def editer(parametres):
@@ -83,11 +94,16 @@ def editer(parametres):
             texte = f.read(-1)
     else:
         with open(os.path.join(DOSSIER,'piece.gabc')) as f:
-            texte = f.read(-1)
+            texte = Template(f.read(-1)).substitute(
+                titre = parametres['titre'] if parametres['titre'] else 'nom',
+                mode = parametres['mode'] if parametres['mode'] else '1.',
+                categorie = parametres['type'] if parametres['type'] else 'antiphona',
+                )
     with open(os.path.join(DOSSIER,'editeur.html')) as f:
         return Template(f.read(-1)).substitute(
                 nom = NOM,
                 texte = texte,
+                enregistrer = s.authentifie('<button type="submit" name="action" value="enregistrer">Enregistrer</button>')
                 )
 
 def traiter(parametres):
@@ -130,6 +146,17 @@ def compiler(parametres,dossier,fichier):
     finally:
         os.chdir(c.PWD)
     os.renames(os.path.join(destination,'partition.pdf'),os.path.join(dossier,fichier))
+
+@s.page
+def telecharger(parametres):
+    gabc = g.Gabc(parametres['texte'])
+    fichierpdf = s.sansaccents(gabc.entetes['name'].replace(' ','_').lower()) + '.' + 'pdf'
+    dossier = os.path.join(c.DATA,'pdf',gabc.entetes['office-part'])
+    compiler(parametres,dossier,fichierpdf)
+    return '<object type="application/pdf" data="/public/data/pdf/{0}/{1}" zoom="page" width="100%" height="100%"></object>'.format(
+                        gabc.entetes['office-part'],fichierpdf
+                        )
+    #compiler()
 
 def enregistrer(parametres):
     gabc = g.Gabc(parametres['texte'])
