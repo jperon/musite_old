b8691695eda9686afe020161ae5260e612a8b171
jperon <cataclop@hotmail.com>
2014-10-21 20:11:42 +0200

    Nettoyage

--- lib/plugins/gregorio.py
+++ lib/plugins/gregorio.py
@@ -1,35 +1,64 @@
-import os,sys,shutil
-import subprocess as sp, traceback as tb
+'''
+Plugin servant à traiter les partitions grégoriennes.
+'''
+import os
+import shutil
+import subprocess as sp
+import traceback as tb
+import ast
 from string import Template
-import config as c, jrnl as l
-import outils as s, auth as a, gabctk as g
+import config as c
+import jrnl as l
+import outils as s
+import auth as a
+import gabctk as g
 import cherrypy as cp
 
 NOM = 'gregorio'
 EXT = 'gabc'
-DOSSIER = os.path.join('lib','plugins','gregorio')
+DOSSIER = os.path.join('lib', 'plugins', 'gregorio')
+
 
 def css():
-    with open(os.path.join(DOSSIER,'style.css')) as f: return f.read(-1)
+    '''Styles de page'''
+    with open(os.path.join(DOSSIER, 'style.css')) as fichier:
+        return fichier.read(-1)
 
 
-def retourner(*arguments,**parametres):
-    l.log('arguments : {}\n paramètres : {}\n'.format(str(arguments),str(parametres)))
+def retourner(*arguments, **parametres):
+    '''Fonction maîtresse, pilotant le reste en fonction des arguments.'''
+    l.log(
+        'arguments : {}\n paramètres : {}\n'.format(
+            str(arguments),
+            str(parametres)
+            )
+        )
     try:
-        return eval(arguments[0])(parametres)
-    except (KeyError,IndexError) as e:
+        return ast.literal_eval(arguments[0])(parametres)
+    except (KeyError, IndexError):
         return accueillir()
 
-def titregabc(f):
-        try:
-            entetes = g.Gabc(g.FichierTexte(f).contenu).entetes
-        except UnicodeDecodeError: return '''<b><i>Erreur d'encodage : {}</i></b>'''.format(f)
-        if 'name' in entetes: return entetes['name']
-        else: return '<b><i>À corriger : fichier sans balise "name" : {}</i></b>'.format(f)
+
+def titregabc(fichier):
+    '''Titre de la partition.'''
+    try:
+        entetes = g.Gabc(g.FichierTexte(fichier).contenu).entetes
+    except UnicodeDecodeError:
+        return '''<b><i>Erreur d'encodage : {}</i></b>'''.format(fichier)
+    if 'name' in entetes:
+        return entetes['name']
+    else:
+        return (
+            '<b><i>'
+            + 'À corriger : fichier sans balise "name" : {}'
+            + '</i></b>'
+            ).format(fichier)
+
 
 @s.page
 def accueillir():
-    racine = os.path.join(c.DATA,EXT)
+    '''Page d'accueil, listant les fichiers gabc.'''
+    racine = os.path.join(c.DATA, EXT)
     dossiers = os.walk(racine)
     page = (
         s.authentifie(
@@ -38,163 +67,254 @@ def accueillir():
             + '</div>'
             )
         + '\n'.join(
-            ['<b>{0}</b>\n<ul id="listechants">\n\t<li>{1}</li>\n</ul>'.format(
-                dossier.replace(racine + os.sep,'').capitalize(),
-                '</li>\n\t<li>'.join(
-                    '<a href="/gregorio/afficher/?fichier={0}">{1}</a>'.format(
-                        os.path.join(dossier.replace(racine + os.sep,''),fichier),
-                        titregabc(os.path.join(dossier,fichier))
-                        )
-                    for fichier in sorted(fichiers,key=lambda x: x.lower())),
+            [
+                (
+                    '<b>{0}</b>\n<ul id="listechants">\n'
+                    + '\t<li>{1}</li>\n</ul>'
+                ).format(
+                    dossier.replace(racine + os.sep, '').capitalize(),
+                    '</li>\n\t<li>'.join(
+                        (
+                            '<a href="/gregorio/afficher/?fichier={0}">{1}</a>'
+                        ).format(
+                            os.path.join(
+                                dossier.replace(racine + os.sep, ''),
+                                fichier
+                                ),
+                            titregabc(os.path.join(dossier, fichier))
+                            )
+                        for fichier in sorted(
+                            fichiers,
+                            key=lambda x: x.lower()
+                            )
+                        ),
                 )
-            for dossier, sousdossiers, fichiers in sorted(dossiers) if len(fichiers)]
+                for dossier, sousdossiers, fichiers
+                in sorted(dossiers) if len(fichiers)
+            ]
             )
         )
     return page
 
+
 @s.page
 def afficher(parametres):
+    '''Aperçu d'une partition, avec en parallèle le Liber Usualis.'''
     fichier = parametres['fichier'].split('/')
-    sousdossier,fichierpdf = fichier[:-1],fichier[-1].replace('.gabc','.pdf')
-    dossier = os.path.join(c.DATA,'pdf',os.sep.join(sousdossier))
-    if not os.path.isfile(os.path.join(dossier,fichierpdf)):
+    sousdossier = fichier[:-1]
+    fichierpdf = fichier[-1].replace('.gabc', '.pdf')
+    dossier = os.path.join(c.DATA, 'pdf', os.sep.join(sousdossier))
+    if not os.path.isfile(os.path.join(dossier, fichierpdf)):
         parametres['papier'] = 'a6paper'
         parametres['taillepolice'] = '12'
         parametres['marge'] = '20px'
-        compiler(parametres,dossier,fichierpdf)
-    with open(os.path.join(DOSSIER,'apercu.html')) as f:
-        return Template(f.read(-1)).substitute(
-                titre = titregabc(os.path.join(c.DATA,'gabc',os.sep.join(fichier))),
-                corps = '<object type="application/pdf" data="/public/data/pdf/{0}/{1}" zoom="page" width="100%" height="100%"></object>'.format(
-                        '/'.join(sousdossier),fichierpdf
-                        ),
-                actions = (
-                    'Mettre en page'
-                    + s.authentifie(
-                        ' − <a href=/gregorio/editer/?fichier={}>Éditer</a>'.format(
-                            parametres['fichier']
-                            )
+        compiler(parametres, dossier, fichierpdf)
+    with open(os.path.join(DOSSIER, 'apercu.html')) as fichier:
+        return Template(fichier.read(-1)).substitute(
+            titre=titregabc(
+                os.path.join(c.DATA, 'gabc', os.sep.join(fichier))
+                ),
+            corps=(
+                '<object type="application/pdf" '
+                + 'data="/public/data/pdf/{0}/{1}" '
+                + 'zoom="page" width="100%" height="100%">'
+                + '</object>'
+                ).format(
+                    '/'.join(sousdossier), fichierpdf
+                    ),
+            actions=(
+                'Mettre en page'
+                + s.authentifie(
+                    (
+                        ' − '
+                        + '<a href=/gregorio/editer/?fichier={}>Éditer</a>'
+                    ).format(
+                        parametres['fichier']
                         )
                     )
                 )
+            )
+
 
 @s.page
 def creer(parametres):
-    with open(os.path.join(DOSSIER,'creation.html')) as f:
-        return Template(f.read(-1)).substitute(
-            nom = NOM,
-            )
-    pass
+    '''Création d'une nouvelle partition.'''
+    with open(os.path.join(DOSSIER, 'creation.html')) as fichier:
+        return Template(fichier.read(-1)).substitute(nom=NOM)
+
 
 @s.page
 def editer(parametres):
+    '''Édition d'une partition.'''
     if 'fichier' in parametres:
         cp.session['anciennom'] = parametres['fichier']
-        with open(os.path.join(c.DATA,EXT,cp.session['anciennom'])) as f:
-            texte = f.read(-1)
+        with open(
+            os.path.join(
+                c.DATA, EXT, cp.session['anciennom']
+                )
+            ) as fichier:
+            texte = fichier.read(-1)
     else:
-        with open(os.path.join(DOSSIER,'piece.gabc')) as f:
-            texte = Template(f.read(-1)).substitute(
-                titre = parametres['titre'] if parametres['titre'] else 'nom',
-                mode = parametres['mode'] if parametres['mode'] else '1.',
-                categorie = parametres['type'] if parametres['type'] else 'antiphona',
+        with open(os.path.join(DOSSIER, 'piece.gabc')) as fichier:
+            texte = Template(fichier.read(-1)).substitute(
+                titre=parametres['titre'] if parametres['titre'] else 'nom',
+                mode=parametres['mode'] if parametres['mode'] else '1.',
+                categorie=(
+                    parametres['type']
+                    if parametres['type']
+                    else 'antiphona'
+                    ),
                 )
-    with open(os.path.join(DOSSIER,'editeur.html')) as f:
-        return Template(f.read(-1)).substitute(
-                nom = NOM,
-                texte = texte,
-                enregistrer = s.authentifie('<button type="submit" name="action" value="enregistrer">Enregistrer</button>')
+    with open(os.path.join(DOSSIER, 'editeur.html')) as fichier:
+        return Template(fichier.read(-1)).substitute(
+            nom=NOM,
+            texte=texte,
+            enregistrer=s.authentifie(
+                '<button type="submit" name="action" value="enregistrer">'
+                + 'Enregistrer</button>'
                 )
+            )
+
 
 def traiter(parametres):
-    return eval(parametres['action'])(parametres)
+    '''Distinction entre la simple mise en page et l'enregistrement
+    d'une partition.'''
+    return ast.literal_eval(parametres['action'])(parametres)
 
-def compiler(parametres,dossier,fichier):
-    if 'texte' in parametres.keys(): contenu = parametres['texte']
+
+def compiler(parametres, dossier, fichier):
+    '''Compilation d'une partition.'''
+    if 'texte' in parametres.keys():
+        contenu = parametres['texte']
     elif 'fichier' in parametres:
-        with open(os.path.join(c.DATA,'gabc',parametres['fichier'])) as f:
-            contenu = f.read(-1)
-    if 'papier' not in parametres: parametres['papier'] = 'a5paper'
-    if 'taillepolice' not in parametres: parametres['taillepolice'] = '12'
-    if 'marge' in parametres: parametres['marge'] = '\\usepackage[margin={0}]{{geometry}}'.format(parametres['marge'])
-    else: parametres['marge'] = ''
+        with open(
+            os.path.join(
+                c.DATA, 'gabc', parametres['fichier']
+                )
+            ) as fichier:
+            contenu = fichier.read(-1)
+    if 'papier' not in parametres:
+        parametres['papier'] = 'a5paper'
+    if 'taillepolice' not in parametres:
+        parametres['taillepolice'] = '12'
+    parametres['marge'] = (
+        '\\usepackage[margin={0}]{{geometry}}'.format(parametres['marge'])
+        ) if 'marge' in parametres else ''
     gabc = 'partition.gabc'
-    commande = ['lualatex','-interaction=batchmode','--shell-escape','partition']
-    destination = os.path.join(c.TMP,NOM)
-    shutil.rmtree(destination,True)
+    commande = [
+        'lualatex',
+        '-interaction=batchmode',
+        '--shell-escape',
+        'partition'
+        ]
+    destination = os.path.join(c.TMP, NOM)
+    shutil.rmtree(destination, True)
     shutil.copytree(
-        os.path.join('modeles',NOM),
+        os.path.join('modeles', NOM),
         destination,
         True
         )
     try:
         os.chdir(destination)
-        with open(os.path.join('gabc',gabc),'w') as f:
-            f.write(contenu)
-        with open('Gabarit.tex','r') as g:
-            with open('partition.tex','w') as f:
-                f.write(
-                    Template(g.read(-1)).substitute(
-                        papier = parametres['papier'],
-                        taillepolice = parametres['taillepolice'],
-                        marge = parametres['marge'],
+        with open(os.path.join('gabc', gabc), 'w') as fichier:
+            fichier.write(contenu)
+        with open('Gabarit.tex', 'r') as gabarit:
+            with open('partition.tex', 'w') as fichier:
+                fichier.write(
+                    Template(gabarit.read(-1)).substitute(
+                        papier=parametres['papier'],
+                        taillepolice=parametres['taillepolice'],
+                        marge=parametres['marge'],
                         )
                     )
         environnement = os.environ.copy()
         environnement['TEXINPUTS'] = 'lib:'
-        sortie,erreurs = sp.Popen(commande,env=environnement,stdout=sp.PIPE,stderr=sp.PIPE).communicate()
+        sortie, erreurs = sp.Popen(
+            commande,
+            env=environnement,
+            stdout=sp.PIPE,
+            stderr=sp.PIPE
+            ).communicate()
+        l.log(
+            'Sortie :\n{}\n\n'.format(sortie)
+            + 'Erreurs :\n{}\n\n'.format(erreurs)
+            )
     finally:
         os.chdir(c.PWD)
-    os.renames(os.path.join(destination,'partition.pdf'),os.path.join(dossier,fichier))
+    os.renames(
+        os.path.join(destination, 'partition.pdf'),
+        os.path.join(dossier, fichier)
+        )
 
-@s.page
+
 def telecharger(parametres):
+    '''Téléchargement d'une partition mise en page.'''
     gabc = g.Gabc(parametres['texte'])
-    fichierpdf = s.sansaccents(gabc.entetes['name'].replace(' ','_').lower()) + '.' + 'pdf'
-    dossier = os.path.join(c.DATA,'pdf',gabc.entetes['office-part'])
-    compiler(parametres,dossier,fichierpdf)
-    return '<object type="application/pdf" data="/public/data/pdf/{0}/{1}" zoom="page" width="100%" height="100%"></object>'.format(
-                        gabc.entetes['office-part'],fichierpdf
-                        )
-    #compiler()
+    fichierpdf = (
+        s.sansaccents(
+            gabc.entetes['name'].replace(' ', '_').lower()
+            )
+        + '.pdf'
+        )
+    dossier = os.path.join(c.DATA, 'pdf', gabc.entetes['office-part'])
+    compiler(parametres, dossier, fichierpdf)
+    return (
+        '<object type="application/pdf" '
+        + 'data="/public/data/pdf/{0}/{1}" '
+        + 'zoom="page" width="100%" height="100%">'
+        + '</object>'
+        ).format(gabc.entetes['office-part'], fichierpdf)
 
+
+@a.reserver(utilisateurs=a.utilisateurs())
 def enregistrer(parametres):
+    '''Enregistrement d'une partition.'''
     gabc = g.Gabc(parametres['texte'])
-    dossier = os.path.join(c.DATA,EXT,gabc.entetes['office-part'])
-    os.makedirs(dossier,exist_ok=True)
-    fichier = s.sansaccents(gabc.entetes['name'].replace(' ','_').lower()) + '.' + EXT
-    emplacement = os.path.join(dossier,fichier)
-    with open(emplacement,'w') as f:
-        f.write(parametres['texte'])
+    dossier = os.path.join(c.DATA, EXT, gabc.entetes['office-part'])
+    os.makedirs(dossier, exist_ok=True)
+    fichier = (
+        s.sansaccents(
+            gabc.entetes['name'].replace(' ', '_').lower()
+            )
+        + '.'
+        + EXT
+        )
+    emplacement = os.path.join(dossier, fichier)
+    with open(emplacement, 'w') as fichier:
+        fichier.write(parametres['texte'])
     try:
         ancienemplacement = os.path.join(
-                    c.DATA,EXT,
-                    cp.session['anciennom'].replace('/',os.sep),
-                    )
+            c.DATA, EXT,
+            cp.session['anciennom'].replace('/', os.sep),
+            )
         if ancienemplacement != emplacement:
             os.remove(ancienemplacement)
-        try: os.remove(
-                    ancienemplacement.replace(
-                        '.' + EXT,'.pdf'
-                        ).replace(
-                        '/' + EXT,'/pdf'
+        try:
+            os.remove(
+                ancienemplacement.replace(
+                    '.' + EXT, '.pdf'
+                    ).replace(
+                        '/' + EXT, '/pdf'
                         )
-                    )
-        except FileNotFoundError: pass
-    except KeyError: pass
+                )
+        except FileNotFoundError:
+            pass
+    except KeyError:
+        pass
     try:
-        os.remove(emplacement.replace(
-                        '.' + EXT,'.pdf'
-                        ).replace(
-                        '/' + EXT,'/pdf'
-                        )
+        os.remove(
+            emplacement.replace(
+                '.' + EXT, '.pdf'
+                ).replace(
+                    '/' + EXT, '/pdf'
                     )
-    except Exception as e:
-        l.log(e)
-        tb.format_exc()
-    raise(cp.HTTPRedirect(
-        '/{nom}/afficher/?fichier={fichier}'.format(
-            nom = NOM,
-            fichier = '/'.join((gabc.entetes['office-part'],fichier))
             )
-        ))
+    except Exception as erreur:
+        l.log(erreur)
+        tb.format_exc()
+    raise cp.HTTPRedirect(
+        '/{nom}/afficher/?fichier={fichier}'.format(
+            nom=NOM,
+            fichier='/'.join((gabc.entetes['office-part'], fichier))
+            )
+        )
--- serveur.py
+++ serveur.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
-import os,sys,re
+'''
+Partie principale : lance le serveur web et appelle les modules
+'''
+import os
+import sys
 from string import Template
 sys.path.insert(0, './etc')
 sys.path.insert(0, './lib')
 sys.path.insert(0, './lib/plugins')
-import html,cherrypy as cp
+import html
+import cherrypy as cp
 import config as c
-import auth as a, outils as s,utilisateurs as u
+import auth as a
+import outils as s
+import utilisateurs as u
 
 PLUGINS = {}
-for m in c.PLUGINS: PLUGINS[m] = __import__(m)
+for m in c.PLUGINS:
+    PLUGINS[m] = __import__(m)
 
-def presenter(retourner,plugin):
+
+def presenter(retourner, plugin):
     '''Chargement des plugins.'''
     @cp.expose
-    def retour(*arguments,**parametres):
+    def retour(*arguments, **parametres):
+        '''Décorateur'''
         cp.session['plugin'] = plugin
-        return retourner(*arguments,**parametres)
+        return retourner(*arguments, **parametres)
     return retour
+
 
 class Site:
     '''Cœur du site : chaque méthode décorée par cherrypy.expose
@@ -26,8 +37,9 @@ class Site:
     @s.page
     def index(self):
         '''Page d'accueil.'''
-        with open('lib/index.html','r') as f:
-            return f.read(-1)
+        with open('lib/index.html', 'r') as fichier:
+            return fichier.read(-1)
+
     @cp.expose
     @s.page
     @a.reserver(utilisateurs=a.utilisateurs())
@@ -35,113 +47,145 @@ class Site:
         '''Page sans grand intérêt, si ce n'est de forcer l'authentification
         des utilisateurs.'''
         return 'Bonjour, {0} !'.format(cp.request.login)
+
     @cp.expose
     def css(self):
         '''Feuille de styles.'''
-        with open(os.path.join('modeles','style.css')) as f: css = Template(f.read(-1))
+        with open(os.path.join('modeles', 'style.css')) as fichier:
+            css = Template(fichier.read(-1))
         try:
             plugin = PLUGINS[cp.session['plugin']].css()
-        except (AttributeError,KeyError):
+        except (AttributeError, KeyError):
             plugin = ''
-        return css.substitute(plugin = plugin)
+        return css.substitute(plugin=plugin)
+
 
 class Admin():
+    '''Pages réservées à l'administration.'''
     @cp.expose
     @s.page
     @a.reserver(groupe='admin')
     def index(self):
+        '''Page d'accueil des administrateurs.'''
         return 'Bonjour, {0} !'.format(cp.request.login)
+
     @cp.expose
     @s.page
     @a.reserver(groupe='admin')
-    def utilisateurs(self,**parametres):
-        utilisateurs = '''<div id="utiliateurs"><b>Utilisateurs :</b>\n<table>{liste}</table>
-            <br>
-            <form method="post" action="/admin/creerutilisateur/">
-                <input name="nom" placeholder="Nom"></input>
-                <br>
-                <input name="mdp" type="password" placeholder="MdP"></input>
-                <br>
-                <input name="mdp_v" type="password" placeholder="MdP (de nouveau)"></input>
-                <br>
-                {texte}
-                <br>
-                <button type="submit">Créer / modifier</button>
-            </form></div>
-            '''.format(
-                liste = '\n'.join(
-                ['<tr><td>{0}&nbsp&nbsp&nbsp</td><td><small><a href=/admin/supprimerutilisateur/?nom={0}>supprimer</a></small></td></tr>'.format(u)\
-                        for u in sorted(a.utilisateurs().keys())]
-                ),
-                texte = parametres['texte'] if 'texte' in parametres else ''
-            )
-        with open(os.path.join('etc','groupes'),'r') as f:
-            groupes = '''
-            <div id="groupes">
-            <script language="javascript" type="text/javascript" src="/public/js/edit_area/edit_area_full.js"></script>
-            <script language="javascript" type="text/javascript">
-                editAreaLoader.init({{
-                    id : "fichier"
-                    ,language: "fr"
-                    ,word_wrap:false
-                    ,show_line_colors:false
-                    ,start_highlight: false
-                }});
-            </script>
-            <b>Groupes :</b>\n<br><br>\n
-            <form method="post" action="/admin/enregistrergroupes/">
-                <textarea name="texte" id="fichier" cols="40" rows="10">{}</textarea>
-                <br>
-                <button type="submit">Enregistrer</button>
-            </form>
-            </div>
-            '''.format(f.read(-1))
-        return '{}<br>{}'.format(utilisateurs,groupes)
+    def utilisateurs(self, **parametres):
+        '''Page de gestion des utilisateurs.'''
+        utilisateurs = (
+            '<div id="utilisateurs"><b>Utilisateurs :</b>\n'
+            + '<table>{liste}</table>\n'
+            + '<br>\n'
+            + '<form method="post" action="/admin/creerutilisateur/">\n'
+            + ' <input name="nom" placeholder="Nom"></input>\n'
+            + ' <br>\n'
+            + ' <input name="mdp" type="password" placeholder="MdP"></input>\n'
+            + ' <br>\n'
+            + ' <input name="mdp_v" type="password" '
+            + 'placeholder="MdP (de nouveau)"></input>\n'
+            + '   <br>\n'
+            + '   {texte}\n'
+            + '   <br>\n'
+            + '   <button type="submit">Créer / modifier</button>\n'
+            + '</form></div>'
+            )\
+            .format(
+                liste='\n'.join(
+                    [
+                        (
+                            '<tr><td>{0}&nbsp&nbsp&nbsp</td><td><small>'
+                            + '<a href=/admin/supprimerutilisateur/?nom={0}>'
+                            + 'supprimer</a>'
+                            + '</small></td></tr>'
+                        ).format(u) for u in sorted(a.utilisateurs().keys())
+                    ]
+                    ),
+                texte=parametres['texte'] if 'texte' in parametres else ''
+                )
+        with open(os.path.join('etc', 'groupes'), 'r') as fichier:
+            groupes = (
+                '<div id="groupes">'
+                + '<script language="javascript" type="text/javascript" '
+                + 'src="/public/js/edit_area/edit_area_full.js"></script>'
+                + '<script language="javascript" type="text/javascript">'
+                + '   editAreaLoader.init({{'
+                + '       id : "fichier"'
+                + '       , language: "fr"'
+                + '       , word_wrap:false'
+                + '       , show_line_colors:false'
+                + '       , start_highlight: false'
+                + '   }});'
+                + '</script>'
+                + '<b>Groupes :</b>\n<br><br>\n'
+                + '<form method="post" action="/admin/enregistrergroupes/">'
+                + '   <textarea name="texte" id="fichier" cols="40" rows="10">'
+                + '{}'
+                + '</textarea>'
+                + '   <br>'
+                + '   <button type="submit">Enregistrer</button>'
+                + '</form>'
+                + '</div>'
+            ).format(fichier.read(-1))
+        return '{}<br>{}'.format(utilisateurs, groupes)
+
     @cp.expose
     @a.reserver(groupe='admin')
-    def creerutilisateur(self,nom,mdp,mdp_v):
+    def creerutilisateur(self, nom, mdp, mdp_v):
+        '''Création d'un nouvel utilisateur.'''
         if mdp == mdp_v:
-            utilisateur = u.Utilisateur(html.escape(nom),html.escape(mdp))
+            utilisateur = u.Utilisateur(html.escape(nom), html.escape(mdp))
             try:
-                utilisateur.ajouter(os.path.join('etc','utilisateurs'))
+                utilisateur.ajouter(os.path.join('etc', 'utilisateurs'))
             except u.UtilisateurExistant:
-                utilisateur.modifier(os.path.join('etc','utilisateurs'))
-            raise(cp.HTTPRedirect('/admin/utilisateurs/'))
+                utilisateur.modifier(os.path.join('etc', 'utilisateurs'))
+            raise cp.HTTPRedirect('/admin/utilisateurs/')
         else:
-            return(self.utilisateurs(texte = 'Les mots de passe ne correspondent pas.'))
+            return self.utilisateurs(
+                texte='Les mots de passe ne correspondent pas.'
+                )
+
     @cp.expose
     @a.reserver(groupe='admin')
-    def enregistrergroupes(self,texte):
-        with open(os.path.join('etc','groupes'),'w') as f:
-            f.write(html.escape(texte))
-        raise(cp.HTTPRedirect('/admin/utilisateurs/'))
+    def enregistrergroupes(self, texte):
+        '''Enregistrement des nouveaux paramètres de groupes'''
+        with open(os.path.join('etc', 'groupes'), 'w') as fichier:
+            fichier.write(html.escape(texte))
+        raise cp.HTTPRedirect('/admin/utilisateurs/')
+
     @cp.expose
     @a.reserver(groupe='admin')
-    def supprimerutilisateur(self,nom):
-        u.Utilisateur(html.escape(nom)).supprimer(os.path.join('etc','utilisateurs'))
-        raise(cp.HTTPRedirect('/admin/utilisateurs/'))
+    def supprimerutilisateur(self, nom):
+        '''Suppression d'un utilisateur.'''
+        u.Utilisateur(html.escape(nom)).supprimer(
+            os.path.join('etc', 'utilisateurs')
+            )
+        raise cp.HTTPRedirect('/admin/utilisateurs/')
+
 
 if __name__ == '__main__':
-    site_config = {
-         '/': {
-             'tools.sessions.on': True,
-             'tools.staticdir.root': c.PWD
-             },
-         '/public': {
-             'tools.staticdir.on': True,
-             'tools.staticdir.dir': './public'
-             },
+    SITE_CONFIG = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': c.PWD
+            },
+        '/public': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+            },
         }
 
-    site = Site()
+    SITE = Site()
     for m in PLUGINS.keys():
-        site_config[m] = {'tools.sessions.on':True}
-        setattr(site,m,presenter(PLUGINS[m].retourner,m))
-    site.admin = Admin()
+        SITE_CONFIG[m] = {'tools.sessions.on': True}
+        setattr(SITE, m, presenter(PLUGINS[m].retourner, m))
+    setattr(SITE, 'admin', Admin())
 
     @s.page
-    def erreur_401(status, message, traceback, version):
+    def erreur_401(*args):
+        '''Si un utilisateur cherche à atteindre une page réservée.'''
         return 'Accès réservé'
     cp.config.update({'error_page.401': erreur_401})
     cp.config.update(c.SERVER_CONFIG)
-    cp.quickstart(site, '/', site_config)
+    cp.quickstart(SITE, '/', SITE_CONFIG)
